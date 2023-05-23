dd2008be855b5cfb686f836cd8d2d2f1eed986af
Ben Hsieh <bqh5026@gmail.com>
2023-05-22 22:31:31 -0500

    Update Logi_Modal object

--- src/modal/Login_Modal.java
+++ src/modal/Login_Modal.java
@@ -1,5 +1,21 @@
 package modal;
 
+import java.sql.Connection;
+
+import bean.Login_Bean;
+import model.DB_Connection;
+
 public class Login_Modal {
+	
+	public boolean check_user_name(Login_Bean obj_Login_Bean) {
+		
+		boolean flag = false;
+		
+		DB_Connection obj_DB_Connection = new DB_Connection();
+		Connection connection = obj_DB_Connection.getConnection(); 
+		
+		return flag; 
+		
+	}
 
 }
