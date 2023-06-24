63bad451de83a028522bee113dc01c0f031b3cc6
Ben Hsieh <bqh5026@gmail.com>
2023-06-24 13:05:35 -0500

    Add user controller

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -1,5 +1,18 @@
 package controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/v1/user")
 public class UserController {
+
+	@PostMapping(path = "/save")
+	public String saveUser() {
+		return "";
+	}
 
 }
