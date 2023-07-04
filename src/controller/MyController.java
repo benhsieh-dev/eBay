cce7442ba4bd2031273b8e859cbcd597afdabf62
Ben Hsieh <bqh5026@gmail.com>
2023-07-04 13:01:30 -0500

    Update app

--- src/controller/MyController.java
+++ src/controller/MyController.java
@@ -1,7 +1,16 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import jakarta.validation.Valid;
+import model.User;
+import model.UserCredential;
 import service.UserService;
 
 @Controller
@@ -18,4 +27,54 @@ public class MyController {
 		return userService;
 	}
 
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String homePage() {
+		return "home";
+	}
+
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public String loginPage(Model model) {
+		model.addAttribute("userCredential", new UserCredential());
+		return "login";
+
+	}
+
+	@RequestMapping(value = "register", method = RequestMethod.GET)
+	public String registerPage(Model model) {
+		model.addAttribute("user", new User());
+		return "register";
+	}
+
+	@RequestMapping(value = "/registerSuccess", method = RequestMethod.POST)
+	public ModelAndView registerSuccess(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return new ModelAndView("register");
+		}
+		getUserService().registerUser(user);
+		ModelAndView modelAndView = new ModelAndView("welcome");
+		modelAndView.addObject("user", user);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = "/loginSuccess", method = RequestMethod.POST)
+	public ModelAndView loginSuccess(@Valid @ModelAttribute("userCredential") UserCredential userCredential,
+			BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return new ModelAndView("login");
+		}
+		ModelAndView modelAndView = new ModelAndView("welcome");
+		User user = getUserService().validateUserCredential(userCredential.getEmail(), UserCredential.getPassword());
+		if (user != null) {
+			modelAndView.addObject("user", user);
+			return modelAndView;
+		} else {
+			modelAndView = new ModelAndView("notFound");
+		}
+		return modelAndView;
+	}
+
+	@ModelAttribute
+	public void headerMessage(Model model) {
+
+	}
 }
--- src/service/UserService.java
+++ src/service/UserService.java
@@ -1,9 +1,13 @@
 package service;
 
 import dto.UserDTO;
+import jakarta.validation.Valid;
+import model.User;
 
 public interface UserService {
 
 	String addUser(UserDTO userDTO);
+
+	void registerUser(@Valid User user);
 
 }
